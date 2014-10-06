fb2384cead4c04c7ec896a81cbaba4f39f643558
Tyler Stark <theirondeveloper@gmail.com>
2014-10-05 21:13:15 -0400

    Added a amd module that requires another amd module

---
+++ scripts/cacheWrapper.js
@@ -0,0 +1,32 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
+// RequireJs File that requires in another amd file
+define(['../scripts/amdCache'], function(Cache) {
+
+	var CacheWrapper = function () {
+		this.cache = new Cache();
+		this.cachedList = [];
+		return this;
+	}
+	CacheWrapper.prototype.set = function(key, value) {
+		this.cache.set(key, value);
+		this.cachedList.push(value);
+		return this;
+	}
+	CacheWrapper.prototype.unset = function(key) {
+		var oldVal = this.cache.storage[key],
+			cacheList = this.cachedList,
+			oldValueIndex = cacheList.indexOf(oldVal);
+
+		cacheList.splice(oldValueIndex, 1);
+		this.cache.unset(key);
+
+		return this;
+	}
+
+	CacheWrapper.prototype.dontTestMe = function() {
+		return "Please don't test me, I'm afraid of Istanbul"
+	}
+
+	return CacheWrapper;
+});
---
+++ test/cacheWrapper.js
@@ -0,0 +1,42 @@
+var requirejs = require('requirejs'),
+	path = __filename,
+	CacheWrapper = require("../scripts/cacheWrapper"),
+	assert = require('assert'),
+	cacheWrapper;
+
+describe ('Cache Wrapper (amd, requiring another amd) ', function() {
+	beforeEach(function() {
+		cacheWrapper = new CacheWrapper();
+	});
+
+	it('should set a value', function() {
+		cacheWrapper.set('dog', 'yorkie');
+		assert.equal(cacheWrapper.cache.storage.dog, 'yorkie');
+		assert.notEqual(cacheWrapper.cache.storage.dog, 'poodle');
+
+		assert.notEqual(cacheWrapper.cachedList.indexOf('yorkie'), -1);
+	});
+
+	it('should unset a value', function() {
+		cacheWrapper.set('dog', 'yorkie');
+		assert.equal(cacheWrapper.cache.storage.dog, 'yorkie');
+		cacheWrapper.unset('dog');
+		assert.equal(cacheWrapper.cache.storage.dog, undefined);
+
+		assert.equal(cacheWrapper.cachedList.indexOf('yorkie'), -1);
+	});
+
+	it('should update the cacheList', function() {
+		assert.equal(cacheWrapper.cachedList.length, 0);
+
+		cacheWrapper.set('dog', 'yorkie');
+		assert.notEqual(cacheWrapper.cachedList.indexOf('yorkie'), -1);
+	});
+
+	it('should remove the correct value from the cacheList', function() {
+		cacheWrapper.set('dog', 'yorkie').set('soda', 'pepsi').set('time', 'night');
+		cacheWrapper.unset('soda');
+
+		assert.equal(cacheWrapper.cachedList.indexOf('pepsi'), -1);
+	});
+});
